d20c846ea1072dda9ce239beabb5a10de083685d
Marc <teste74@hotmail.fr>
2022-03-14 15:38:54 +0100

    Update AUTOINSTALL.sh

--- BASH/AUTOINSTALL.sh
+++ BASH/AUTOINSTALL.sh
@@ -8,18 +8,14 @@ case "$1" in
   # --------------------------------------------------------------------------------------------- #
   # Afficher les Disques-dur:
   fdisk -l | grep "^/dev"  | cut -d " " -f 1
-  
   echo -n "Nom du disque-dur: ";
   read DISK;
   echo -n "Taille de la partition BOOT: ";
   read SIZE_BOOT;
-
   echo -n "Taille de la partition SYSTEM: ";
   read SIZE_SYST;
-
   echo -n "Taille de la partition HOME: ";
   read SIZE_HOME;
-
   echo -n "Nom du Volume Group: ";
   read LVM_NAMEVG;
   ;;
