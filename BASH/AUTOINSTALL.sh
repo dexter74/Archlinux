4ee72447c663feb3a72795e2f46ac20ef5910bdc
Marc <teste74@hotmail.fr>
2022-03-14 15:43:58 +0100

    Update AUTOINSTALL.sh

--- BASH/AUTOINSTALL.sh
+++ BASH/AUTOINSTALL.sh
@@ -1,23 +1,29 @@
 
 #!/bin/sh
 clear;
+
+###################################################################################################################################################
+# Fonctions #
+#############
+DISK(){
+  # Afficher les Disques-dur:
+  fdisk -l | grep "^/dev"  | cut -d " " -f 1 ;
+  echo -n "Nom du disque-dur: "              ; read DISK       ;
+  echo -n "Taille de la partition BOOT: "    ; read SIZE_BOOT  ;
+  echo -n "Taille de la partition SYSTEM: "  ; read SIZE_SYST  ;
+  echo -n "Taille de la partition HOME: "    ; read SIZE_HOME  ;
+  echo -n "Nom du Volume Group: "            ; read LVM_NAMEVG ;
+}
+
+
+
 ###################################################################################################################################################
 case "$1" in
  # ---------------------------------------------------------------------------------------------------------------------------------------------- #
  0)
   # --------------------------------------------------------------------------------------------- #
-  # Afficher les Disques-dur:
-  fdisk -l | grep "^/dev"  | cut -d " " -f 1
-  echo -n "Nom du disque-dur: ";
-  read DISK;
-  echo -n "Taille de la partition BOOT: ";
-  read SIZE_BOOT;
-  echo -n "Taille de la partition SYSTEM: ";
-  read SIZE_SYST;
-  echo -n "Taille de la partition HOME: ";
-  read SIZE_HOME;
-  echo -n "Nom du Volume Group: ";
-  read LVM_NAMEVG;
+  # Fonction DISK
+  DISK
   ;;
 
  # ---------------------------------------------------------------------------------------------------------------------------------------------- #
